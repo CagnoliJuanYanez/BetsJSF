701b2546eefd8db36d7b5a9185da242a5fe04371
Francisco Cagnoli <FC238266@fi365.ort.edu.uy>
2022-11-29 15:01:39 +0100

    CreateQuesitonBean fix

--- src/main/java/modelo/bean/CreateQuestionBean.java
+++ src/main/java/modelo/bean/CreateQuestionBean.java
@@ -1,5 +1,37 @@
 package modelo.bean;
 
+import java.util.Date;
+import java.util.List;
+
+import org.primefaces.event.SelectEvent;
+
+import businessLogic.BLFacade;
+import businessLogic.BLFacadeImplementation;
+import dataAccess.DataAccess;
+import domain.Event;
+
 public class CreateQuestionBean {
+	public List<Event> events;
+	private BLFacade blFacade;
+
+	public CreateQuestionBean() {
+		blFacade = new BLFacadeImplementation(new DataAccess());
+	}
+	
+	public void query(SelectEvent event) {
+		this.events = blFacade.getEvents((Date) event.getObject());
+	}
+
+	public List<Event> getEvents() {
+		return events;
+	}
+
+	public void setEvents(List<Event> events) {
+		this.events = events;
+	}
+	
+
 
 }
+
+
