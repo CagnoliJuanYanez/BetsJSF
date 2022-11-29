6f94fcaf0a9a97a71cffeae74dd59173be5559ac
Francisco Cagnoli <FC238266@fi365.ort.edu.uy>
2022-11-29 17:37:49 +0100

    create question functionality finished

--- src/main/java/modelo/bean/CreateQuestionBean.java
+++ src/main/java/modelo/bean/CreateQuestionBean.java
@@ -9,10 +9,17 @@ import businessLogic.BLFacade;
 import businessLogic.BLFacadeImplementation;
 import dataAccess.DataAccess;
 import domain.Event;
+import exceptions.EventFinished;
+import exceptions.QuestionAlreadyExist;
 
 public class CreateQuestionBean {
 	public List<Event> events;
 	private BLFacade blFacade;
+	public Event event;
+	public String question;
+	public float betMinimum;
+	public String result;
+
 
 	public CreateQuestionBean() {
 		blFacade = new BLFacadeImplementation(new DataAccess());
@@ -20,6 +27,22 @@ public class CreateQuestionBean {
 	
 	public void query(SelectEvent event) {
 		this.events = blFacade.getEvents((Date) event.getObject());
+	}
+	
+	public void addQuestion() {
+		try {
+			blFacade.createQuestion(event, question, betMinimum);
+			this.result = "Question added successfuly!";
+		} catch (EventFinished e) {
+			System.out.print("You cannot add a question to this event, this event has already finished.");
+			this.result = "You cannot add a question to this event, this event has already finished.";
+		} catch (QuestionAlreadyExist e) {
+			System.out.print("You cannot add this question to this event, this question already exists.");
+			this.result = "You cannot add this question to this event, this question already exists.";
+		} catch (Exception e) {
+			System.out.print("Error has ocurred:" + e.getMessage());
+			this.result = "Error has ocurred:" + e.getMessage();
+		}
 	}
 
 	public List<Event> getEvents() {
@@ -30,6 +53,37 @@ public class CreateQuestionBean {
 		this.events = events;
 	}
 	
+	public Event getEvent() {
+		return event;
+	}
+	
+	public void setEvent(Event ev) {
+		this.event = ev;
+	}
+	
+	public String getQuestion() {
+		return question;
+	}
+	
+	public void setQuestion(String q) {
+		this.question = q;
+	}
+	
+	public float getBetMinimum() {
+		return betMinimum;
+	}
+	
+	public void setBetMinimum(float bm) {
+		this.betMinimum = bm;
+	}
+	
+	public String getResult() {
+		return result;
+	}
+	
+	public void setResult(String res) {
+		this.result = res;
+	}
 
 
 }
